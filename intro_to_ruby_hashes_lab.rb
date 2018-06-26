87a2e159d395da3ee61b023d3ea402a7cc6784f6
Hayden Betts <haydenbetts@gmail.com>
2018-06-26 19:12:47 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,15 +17,10 @@ def monopoly_with_second_tier
 	monopoly[:railroads] = {}
 	monopoly[:railroads][:pieces] = 4
 	monopoly[:railroads][:names] = {}
-
 	monopoly
 end
 
 def monopoly_with_third_tier
-	monopoly = {}
-	monopoly[:railroads] = {}
-	monopoly[:railroads][:pieces] = 4
-	monopoly
 end
 
 def monopoly_with_fourth_tier
