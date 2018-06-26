c8c67c4fe2d6f1194fe1980d668454abef4c829a
Hayden Betts <haydenbetts@gmail.com>
2018-06-26 19:19:51 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,7 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-	monopoly = monopoly_with_second_tier
+	monopoly = monopoly
 	monopoly[:railroads][:names] = {}
 	monopoly[:railroads][:rent_in_dollars] = {}
 	monopoly
